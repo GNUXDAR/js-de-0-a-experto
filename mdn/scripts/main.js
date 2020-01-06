47789061d145a3c4c1d57c4676dd51d35861cf1a
gnuxdar <gnuxdar@gmail.com>
2020-01-05 22:39:45 -0500

    reubicados ficheros

--- mdn/scripts/main.js
+++ mdn/scripts/main.js
@@ -1,2 +1,6 @@
 var miTitulo = document.querySelector('h1');
 miTitulo.innerHTML = 'Hello world!';
+
+document.querySelector('img').onclick = function() {
+    alert('Ouch! Deja de pincharme! soy un panda rojo en peligro de extinsion');
+}
