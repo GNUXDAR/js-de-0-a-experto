67f315e428b872d84d3666850b8d12ad2030a588
gnuxdar <gnuxdar@gmail.com>
2020-01-14 21:22:36 -0500

    bucle do while

---
+++ Udemy/Master_en_JavaScript/scripts/10-do-while.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//Bucle do while
+/* Bucle es una estructura de control que se repite N veces
+el do while se ejecutara primeramente y despues evalua mediante la condicion
+y se repetira si la condicion se cumple
+si no se cumple sale del bucle
+*/
+
+ var age = 20;
+
+ do{
+ 	console.log('Tienes de edad: '+age);
+ 	age++;
+ }while(age <= 25);
