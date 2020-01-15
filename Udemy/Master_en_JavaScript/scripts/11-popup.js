f6ee13158f0d6480e98ba2e758deb78eabf77849
gnuxdar <gnuxdar@gmail.com>
2020-01-14 21:51:32 -0500

    popup en javaScript

---
+++ Udemy/Master_en_JavaScript/scripts/11-popup.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// alert: es para mostrar un mensaje por pantalla
+alert('alert');
+
+//prompt:recoge datos por parte del usuario
+var myResult = prompt('Que hora es?', '21:00'); //el segundo parametro es el default y es opcional
+alert(myResult);
+
+
+//confirm: es 
+var result = confirm('Esta seguro?');
+alert(result); //en la variable result almaceno lo que elija el usuario
+
+
+//
