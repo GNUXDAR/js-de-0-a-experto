a04497ab2fe523fc0957f05bc3384361742b78dd
gnuxdar <gnuxdar@gmail.com>
2020-01-15 23:05:01 -0500

    Ejercicio #5

---
+++ Udemy/Master_en_JavaScript/scripts/16-ejercicio-5.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+	#ejercicio 5
+	que nos diga si un numero es par o impar
+	1. ventana prompt
+	2. si no es valido que nos pida de nuevo el numero
+*/
+
+
+var num = parseInt(prompt('Ingrese su numero', 0));
+
+while(num < 0 || isNaN(num)){
+	num = parseInt(prompt('Ingrese su numero', 0));
+}
+
+if (num % 2 === 0)
+	alert(num + ' Es un numero par')
+
+if (num % 2 != 0)
+	alert(num + ' Es un numero impar')
