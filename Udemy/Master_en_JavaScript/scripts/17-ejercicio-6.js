3629fe7b37eb945e095ed9850c14f736790d705e
gnuxdar <gnuxdar@gmail.com>
2020-01-16 21:45:24 -0500

    Ejercicio #6

--- Udemy/Master_en_JavaScript/scripts/17-ejercicio-6.js
+++ Udemy/Master_en_JavaScript/scripts/17-ejercicio-6.js
@@ -6,11 +6,14 @@
 
 var num = parseInt(prompt('Ingrese el numero que quiere que se haga la tabla de multiplicar: ', 0));
 
+var title = document.getElementsByTagName('h1')[0];
+title.innerHTML += '<br> Tabla de Multiplicar de '+num;
+
 while(num <= 0 || isNaN(num)){
 	num = parseInt(prompt('Ingrese el numero que quiere que se haga la tabla de multiplicar: ', 0));
 }
 
 for (var i = 0; i <= 10; i++) {
-	var result = num * i;
+	let result = num * i;
 	document.write(num + ' * ' + i + ' = ' +result +'<br>');
 }
