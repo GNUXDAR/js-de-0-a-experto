b57aaffbb4aa2074094acbb3dcf0d7b5d0c49d3e
gnuxdar <gnuxdar@gmail.com>
2020-02-02 10:46:18 -0500

    Arrays multidimensionales, agregar elementos, eliminar el ultimo elemento de array y convertirlo en string

--- Udemy/Master_en_JavaScript/scripts/30-arrays-multi.js
+++ Udemy/Master_en_JavaScript/scripts/30-arrays-multi.js
@@ -12,3 +12,24 @@ var cine = [category, films]; //una array de arrays
 console.log(cine);
 console.log(cine[0][1]);//accediendo al primer array
 console.log(cine[1][1]);//accediendo al segundo array
+
+//agregar elementos a aun array
+films.push('Red social');
+console.log(films);
+
+//interaccion con arrays
+var elemento = "";
+
+do {
+	elemento = prompt('Agrega una pelicula \n para terminar agregegue END');
+	films.push(elemento);
+}while(elemento != 'END');
+films.pop();	//elimina el ultimo elemento de un Array
+
+console.log(films);
+document.write(films);
+
+//convertir un Array en string
+var films_string = films.join();
+console.log(films_string);
+console.log(typeof films_string);
