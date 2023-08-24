cf6d464479c69b51725b5e7c0ecd2c87f903f36c
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-24 09:02:18 -0500

    [NEW] directives to v-if, v-else, v-show

---
+++ Vue/Vuejs-Docker/helloworld/src/components/ExampleSelective.vue
@@ -0,0 +1,15 @@
+<!-- Directivas v-show, v-if, v-else -->
+<template>
+    <div>
+        <h2 v-if="age < 18 && age != 33">Eres Menor de Edad</h2>
+        <h2 v-else>Eres Mayor de Edad</h2>
+        <h2 v-show="age == 33">Tengo 33 años</h2>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    let age = 33
+</script>
+
+<style scoped>
+</style>
