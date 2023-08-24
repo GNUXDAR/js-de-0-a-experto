1411659a525967cde0a7bfe8512de52a9770ae5f
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-24 09:02:34 -0500

    [NEW] directives to v-for

---
+++ Vue/Vuejs-Docker/helloworld/src/components/ExampleFor.vue
@@ -0,0 +1,39 @@
+<!-- Directivas v-for -->
+<template>
+    <ul>
+        <li v-for="(country, index) in countries" :key="index">{{ country }}</li>
+    </ul>
+
+    <ul>
+        <li v-for="(code, index) in code_countries" :key="index">{{ code.code }}</li>
+    </ul>
+
+</template>
+
+<script lang="ts" setup>
+
+import { Ref, ref } from 'vue';
+
+let countries:Ref<Array<string>> = ref([
+    'Venezuela', 'Ecuador', 'colombia'
+])
+
+let code_countries = ref([
+    {
+        code: 've',
+        name: 'Venezuela'
+    },
+    {
+        code: 'ec',
+        name: 'Ecuador'
+    },
+    {
+        code: 'co',
+        name: 'Colombia'
+    }
+])
+</script>
+
+
+<style scoped>
+</style>
