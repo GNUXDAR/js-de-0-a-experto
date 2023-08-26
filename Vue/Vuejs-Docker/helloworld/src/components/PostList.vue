fa4d4e880d0bdc188047fba4900b438555941dc6
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-26 18:55:34 -0500

    [NEW] New component to show various post of the Component children PostDetail

---
+++ Vue/Vuejs-Docker/helloworld/src/components/PostList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <PostDetailVue />
+        <PostDetailVue />
+        <PostDetailVue />
+        <PostDetailVue />
+    </div>
+
+</template>
+
+<script lang="ts" setup>
+  import PostDetailVue from './PostDetail.vue';
+
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex: wrap;
+    justify-content: space-around;
+}
+</style>
