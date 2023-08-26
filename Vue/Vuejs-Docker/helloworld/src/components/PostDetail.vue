061f6e6a5b275308ed2ba8565384aff02d0c9f6b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-26 18:56:25 -0500

    [NEW] New component children of PostList, when show detail post

---
+++ Vue/Vuejs-Docker/helloworld/src/components/PostDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="post">
+        <h3>Titulo</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae molestias, obcaecati soluta ea quae, ullam pariatur debitis fugit doloremque quia veniam? Assumenda temporibus, accusantium vel sapiente accusamus inventore rem voluptate.</p>
+    </div>
+    
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+.post {
+    width: auto;
+    height: auto;
+    background-color: aquamarine;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+
+}
+</style>
