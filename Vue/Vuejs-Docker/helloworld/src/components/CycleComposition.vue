565fd9a9120d37691faf2c650d374b41791938f7
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-26 17:45:20 -0500

    [NEW] component Composition to cycle life

---
+++ Vue/Vuejs-Docker/helloworld/src/components/CycleComposition.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Composition API</p>
+</template>
+
+<script lang="ts" setup>
+// uso de composition API
+import { onMounted, onUpdated } from 'vue'
+
+onMounted(() => {
+    console.log('Mounted in composition')
+})
+
+onUpdated(() => {
+    console.log('Updated in composition')
+})
+
+</script>
+
+<style scoped>
+</style>
