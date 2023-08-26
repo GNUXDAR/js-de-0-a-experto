aaafb5f43fa449e45e1cd152b16bb5b6d88b578b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-26 17:46:06 -0500

    [NEW] component Option to cycle life

---
+++ Vue/Vuejs-Docker/helloworld/src/components/CycleOption.vue
@@ -0,0 +1,37 @@
+<template>
+    <p>Option API</p>
+</template>
+
+<script lang="ts">
+// uso de option API
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'CycleOption',
+    // data() {
+    //     return {
+    //     }
+    // },
+    // methods: {
+
+    // },
+    created() {
+        console.log('Created in option api');
+        
+    },
+    mounted() {
+        console.log('Mounted in option api');
+    },
+    updated() {
+        console.log('Updated in option api');
+    },
+    unmounted() {
+        console.log('Unmounted in option api');
+    },
+
+
+})
+</script>
+
+<style scoped>
+</style>
