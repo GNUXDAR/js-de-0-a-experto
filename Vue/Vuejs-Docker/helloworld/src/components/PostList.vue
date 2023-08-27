e9942020638f52e590b49cd274690e666642a958
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-26 19:27:43 -0500

    [UPD] Comunication descend, father to children. this father

--- Vue/Vuejs-Docker/helloworld/src/components/PostList.vue
+++ Vue/Vuejs-Docker/helloworld/src/components/PostList.vue
@@ -1,15 +1,42 @@
 <template>
     <div class="container">
+        <!-- <PostDetailVue />
         <PostDetailVue />
         <PostDetailVue />
-        <PostDetailVue />
-        <PostDetailVue />
+        <PostDetailVue /> -->
+
+        <!-- enviar dato al componente hijo "PostDetail" mediante las props title="elm.title" content="elm.content" img="mi-img.png" -->
+        <!-- Comunicacion Descendiente -->
+        <PostDetailVue v-for="elm in info" :key="elm.title" :title="elm.title" :content="elm.content" />
     </div>
 
 </template>
 
 <script lang="ts" setup>
   import PostDetailVue from './PostDetail.vue';
+
+  interface Post {
+    title: string,
+    content: string
+  }
+
+  let info:Array<Post> = [
+    {
+        title: "Este es el post 1",
+        content: "Texto de post 1"
+    }, 
+    {
+        title: "Este es el post 2",
+        content: "Texto de post 2"
+    },
+    {
+        title: "Este es el post 3",
+        content: "Texto de post 3"
+    },
+    {
+        title: "Este es el post 4",
+        content: "Texto de post 4"
+    },]
 
 </script>
 
