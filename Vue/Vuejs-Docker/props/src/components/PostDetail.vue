5547a67540133890f8f23be79531710162544f80
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-28 16:18:21 -0500

    [UPD] typado

--- Vue/Vuejs-Docker/props/src/components/PostDetail.vue
+++ Vue/Vuejs-Docker/props/src/components/PostDetail.vue
@@ -2,20 +2,30 @@
     <div class="post">
         <h3>{{ title }}</h3>
         <p>{{ content }}</p>
+        <p>{{ props }}</p>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, defineProps } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'PostDetail',
     props: {
         id: Number,
-        title: String,
+        title: {
+            type: String,
+            required: true
+        },
         content: {
             type: String,
+            required: false,
             dafault: "Aqui contenido por defecto"
+        }
+    },
+    setup(props){
+        return {
+            props
         }
     }
 })
--- Vue/Vuejs-Docker/props/src/components/PostList.vue
+++ Vue/Vuejs-Docker/props/src/components/PostList.vue
@@ -20,25 +20,26 @@ export default defineComponent({
             content: string
         }
 
+        // Array de objetos validado por la interface
         let info:Array<Post> = [
             {
                 id: 1,
-                title: "Este es el post 1",
+                title: "Este es el title 1",
                 content: "Texto de post 1"
             }, 
             {
                 id: 2,
-                title: "Este es el post 2",
+                title: "Este es el title 2",
                 content: "Texto de post 2"
             },
             {
                 id: 3,
-                title: "Este es el post 3",
+                title: "Este es el title 3",
                 content: "Texto de post 3"
             },
             {
                 id: 4,
-                title: "Este es el post 4",
+                title: "Este es el title 4",
                 content: "Texto de post 4"
             }]
 
