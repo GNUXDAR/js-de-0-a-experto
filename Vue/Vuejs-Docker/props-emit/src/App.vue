3e5a9a91057c2f82a5a226155b93116336cb16f8
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-28 20:32:18 -0500

    [UPD] chaged title

--- Vue/Vuejs-Docker/props-emit/src/App.vue
+++ Vue/Vuejs-Docker/props-emit/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <h2>Props</h2>
+  <h2>props && emit</h2>
   <p>Los props en Vue son propiedades personalizadas que se pueden pasar de un componente padre a un componente hijo. Permiten la comunicación entre componentes al permitir que el componente padre envíe datos al componente hijo para su uso. Los props son valores inmutables en el componente hijo, lo que significa que el componente hijo no puede modificar directamente los valores de los props; en cambio, los utiliza como datos de solo lectura. Los props son una parte fundamental de la comunicación vertical en Vue y son esenciales para la construcción de aplicaciones basadas en componentes reutilizables.</p>
   <PostList />
 </template>
