5889db8f518f51963a925979138c556204a5e9f9
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-29 16:54:02 -0500

    [NEW] computed property

--- Vue/Vuejs-Docker/props-emit/src/App.vue
+++ Vue/Vuejs-Docker/props-emit/src/App.vue
@@ -7,17 +7,26 @@
   <h2 class="emit">emit</h2>
   <p>Emitir en Vue es un mecanismo que permite a un componente hijo comunicarse con su componente padre. Utiliza la función `emit` para enviar eventos personalizados desde el componente hijo al componente padre. Estos eventos personalizados pueden llevar datos y desencadenar funciones en el componente padre, lo que facilita la interacción y la comunicación entre componentes en una aplicación Vue. El componente hijo emite un evento y el componente padre lo "escucha" y responde según sea necesario, lo que permite una comunicación efectiva y flexible entre componentes en la jerarquía de Vue.</p>
   <PostList />
+
+  <hr>
+  <h2>Propiedades Computadas</h2>
+  <p>Las propiedades computadas Vue son una de las características más potentes del framework. Permiten establecer comparaciones y lógica elaborada que se reevalúa cada vez que uno de sus valores a comprobar cambia, asegurando su reactividad.</p>
+
+  <p>Las propiedades computadas son un tipo de propiedad especial que se declara como una función, y que se ejecuta cuando accedemos a la propiedad con dicho nombre.</p>
+  <ComputedExample />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import PostList from './components/PostList.vue'
+import ComputedExample from './components/ComputedExample.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    PostList
-  }
+    PostList,
+    ComputedExample
+}
 });
 </script>
 
---
+++ Vue/Vuejs-Docker/props-emit/src/components/ComputedExample.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <!-- revertir el texto que pasemos -->
+        <input type="text" v-model="message">
+        <br>
+        <code>Revertir el texto que se escriba aca, mediante una propiedad computada</code>
+        <h3>{{ reverse }}</h3>
+    </div>
+</template>
+
+<script lang="ts">
+// options API
+// import { defineComponent } from 'vue';
+
+// export default defineComponent({
+//     name:'ComputedExample',
+//     data() {
+//         return {
+//             message: ''
+//         }
+//     },
+//     computed: {
+//         reverse: function () {
+//             return this.message.split('').reverse().join('')
+//         }
+//     },
+// })
+
+// Composition APi
+
+    import { defineComponent, Ref, ref, computed } from 'vue';
+
+    export default defineComponent({
+        name: 'ComputedExample',
+        setup() {
+            let message:Ref<string> = ref('aa')
+            const reverse = computed(()=> message.value.split('').reverse().join(''))
+            return { message, reverse }
+        }
+
+    })
+
+</script>
+
+<style scoped>
+</style>
