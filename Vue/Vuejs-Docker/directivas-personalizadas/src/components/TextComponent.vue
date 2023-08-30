739ad933e59fb271180a260645f3efc2a9805eaa
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-30 18:19:58 -0500

    [NEW] Custom Directives with 'el, binding'

--- Vue/Vuejs-Docker/directivas-personalizadas/src/components/TextComponent.vue
+++ Vue/Vuejs-Docker/directivas-personalizadas/src/components/TextComponent.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="container">
         <span v-font-size>Directivas Personalizadas</span>
-        <p >
-            En Vue 3, puedes crear directivas personalizadas para extender la funcionalidad de Vue.js según tus necesidades específicas. Las directivas personalizadas te permiten interactuar directamente con el DOM, modificar elementos, escuchar eventos y aplicar comportamientos personalizados a elementos HTML. Aquí hay una descripción general de cómo puedes crear una directiva personalizada en Vue 3:
-        </p>
+        <br>
+        <span v-custom-size="60">Directivas Personalizadas</span>
+
+        <p>En Vue 3, puedes crear directivas personalizadas para extender la funcionalidad de Vue.js según tus necesidades específicas. Las directivas personalizadas te permiten interactuar directamente con el DOM, modificar elementos, escuchar eventos y aplicar comportamientos personalizados a elementos HTML. Aquí hay una descripción general de cómo puedes crear una directiva personalizada en Vue 3:        </p>
         
         <p>Para crear una directiva personalizada, puedes usar el método app.directive() proporcionado por la instancia de Vue. Por ejemplo, en el archivo principal de tu aplicación Vue, puedes definir una directiva personalizada de la siguiente manera:</p>
     
-        <code><p>
+        <code>
+            <p>
             app.directive('mi-directiva', { <br>
                 // Configuración de la directiva<br>
             });
-                        </p>
-</code>
-    </div>
-    
+            </p>
+        </code>
 
+    </div>
 </template>
 
 <script lang="ts" setup>
