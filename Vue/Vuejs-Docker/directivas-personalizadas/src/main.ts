351be27a0b7a24d0270787220da6467f3ff8c414
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-30 17:59:22 -0500

    [NEW] Custom Directives with 'el'

--- Vue/Vuejs-Docker/directivas-personalizadas/src/main.ts
+++ Vue/Vuejs-Docker/directivas-personalizadas/src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// estructura de directiva personalizada
+const app = createApp(App)
+
+app.directive(
+    'font-size', // nombre de mi directiva
+    {
+        beforeMount: (el) => {
+            el.style.fontSize = '2em'
+            el.style.color = 'red'
+            el.style.fontWeight = 'bold'
+        }
+    }
+    )
+
+
+app.mount('#app')
+
