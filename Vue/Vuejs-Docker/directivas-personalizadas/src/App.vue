dc1e4d941c827e22ce01fb25e25ef2eb418dad0a
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-08-30 16:39:01 -0500

    [NEW] Property Custom

---
+++ Vue/Vuejs-Docker/directivas-personalizadas/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h2>Directivas Personalizadas</h2>
+  
+
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #41b883;
+  margin-top: 60px;
+}
+</style>
