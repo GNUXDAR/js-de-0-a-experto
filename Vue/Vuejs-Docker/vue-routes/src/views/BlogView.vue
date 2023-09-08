555ce3413e7d01fab2ded2aa7a74cfb8da31030a
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-08 17:51:05 -0500

    [NEW] component to list post

---
+++ Vue/Vuejs-Docker/vue-routes/src/views/BlogView.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Blog</h1>
+    <ul>
+        <li>
+            <router-link>Mi Primer Post</router-link> 
+        </li>
+        <li>
+            <router-link >Segundo Post</router-link>
+        </li>
+        <li>
+            <router-link >Tercer Post</router-link>
+        </li>
+        <li>
+            <router-link >Cuarto Post</router-link>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
