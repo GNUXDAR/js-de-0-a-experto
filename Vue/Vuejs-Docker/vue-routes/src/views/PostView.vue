1f92ecd104a97cb8aabad04d09263f33b9513b2d
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-09 13:36:48 -0500

    [UPD] Reading for params o the component

--- Vue/Vuejs-Docker/vue-routes/src/views/PostView.vue
+++ Vue/Vuejs-Docker/vue-routes/src/views/PostView.vue
@@ -1,8 +1,18 @@
 <template>
     <h1>Post</h1>
+    <p>Este es el post Numero {{ postId }}</p>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+let postId = ref('')
+
+onMounted(() => {
+    const route = useRoute()
+    postId.value = route.params.postid
+})
 </script>
 
 <style scoped>
