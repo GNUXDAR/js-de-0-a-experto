0bc55dfee2fd2e4ca2f803402a1b57cbe86d0dae
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-12 13:06:37 -0500

    [UPD] use of service jsonplaceholder to podt detail

--- Vue/Vuejs-Docker/vue-services/src/services/PostService.ts
+++ Vue/Vuejs-Docker/vue-services/src/services/PostService.ts
@@ -5,14 +5,20 @@ import InterfacePost from '@/interfaces/InterfacePost'
 
 class PostService {
     private posts:Ref<Array<InterfacePost>>
+    private post:Ref<InterfacePost>
 
     constructor() {
         this.posts = ref<Array<InterfacePost>>([])
+        this.post = ref<InterfacePost>({})
         
     }
 
     getPosts():Ref<Array<InterfacePost>> {
         return this.posts
+    }
+
+    getPost():Ref<InterfacePost> {
+        return this.post
     }
 
     async fetchAll():Promise<void> {
@@ -24,6 +30,17 @@ class PostService {
 
         }catch(error){
             console.log(error)
+        }
+    }
+
+    async fetchById(id: string | Array<string>): Promise<void> {
+        try{
+            const url = `https://jsonplaceholder.typicode.com/posts/${id}`
+            const response = await fetch(url)
+            const json = await response.json()
+            this.post.value = await json
+        }catch(error){
+            console.log(error);
         }
     }
 }
