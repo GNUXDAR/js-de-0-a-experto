2aa29fe6f2037d3986e04f3a497ccde291f87239
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-12 10:28:24 -0500

    [UPD] add RouterView in App.vue

--- Vue/Vuejs-Docker/vue-services/src/App.vue
+++ Vue/Vuejs-Docker/vue-services/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <PostList msg="Services"/>
+  <!-- <PostList msg="Services"/> -->
+  <RouterView msg="Services" />
 </template>
 
 <script lang="ts" setup>
-import PostList from './components/PostList.vue';
+// import PostList from './components/PostList.vue';
+// import PostListView from './views/PostListView.vue';
+// ya no lo necesitaremos, porque usamoe el reouterView
 
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primaryColor;
   margin-top: 60px;
 }
 </style>
