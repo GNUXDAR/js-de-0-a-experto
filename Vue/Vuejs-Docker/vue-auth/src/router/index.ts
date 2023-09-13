ad76b1513ee582c91639df384158187044ef8487
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-13 14:20:48 -0500

    [UPD] create route to component AuthView

--- Vue/Vuejs-Docker/vue-auth/src/router/index.ts
+++ Vue/Vuejs-Docker/vue-auth/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuthView from '../views/AuthView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: AuthView
   },
   {
     path: '/about',
