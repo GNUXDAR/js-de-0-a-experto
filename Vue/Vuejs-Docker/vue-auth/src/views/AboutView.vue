a0a5215dcdcdd655d33a28bd60877902def4eba1
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-13 14:22:31 -0500

    [NEW] component AuthView

--- Vue/Vuejs-Docker/vue-auth/src/views/AboutView.vue
+++ Vue/Vuejs-Docker/vue-auth/src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <p>Estaremos probando varios elementos
+        <ul>
+          <li>router</li>
+        </ul>
+      </p>
+      <h3>Credenciales de el Servicio</h3>
+      <p>https://hfp69ilv.directus.app/auth/login</p>
+      <p><b>user:</b> testuser@gogodev.net</p>
+      <p><b>pass:</b> 12345678</p>
   </div>
 </template>
---
+++ Vue/Vuejs-Docker/vue-auth/src/views/AuthView.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Auth View</h1>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+            </div>
+            <button type="submit" class="btn btn-primary mt-2">Submit</button>
+            <!-- {{ email }}
+            {{ password }} -->
+        </form>
+    </div>
+
+</template>
+
+<script lang="ts" setup>
+import { ref, Ref } from 'vue'
+
+    let email:Ref = ref("")
+    let password:Ref = ref("")
+
+</script>
+
+<style>
+</style>
