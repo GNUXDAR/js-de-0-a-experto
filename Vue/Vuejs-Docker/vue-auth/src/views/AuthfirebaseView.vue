2396baddda4fc8fd14a77b40a7ed7f18e35a7986
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-14 17:27:58 -0500

    [NEW] component to AuthfirebaseView connect firebase

---
+++ Vue/Vuejs-Docker/vue-auth/src/views/AuthfirebaseView.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>Auth View with Firebase</h1>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    v-model="email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+            </div>
+            <button type="submit" class="btn btn-primary mt-2" @click.prevent="authUser">Submit</button>
+            <!-- {{ email }}
+            {{ password }} -->
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, Ref } from 'vue'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+let email: Ref = ref("")
+let password: Ref = ref("")
+
+const authUser = async () => {
+    const auth = getAuth()
+    signInWithEmailAndPassword(auth, email.value, password.value).then( () => {
+        alert('Exito')
+    }).catch( (error) => {
+        alert('failed' + error.message)
+    })
+}
+
+
+
+</script>
+
+<style></style>
