1fbb357a54704b002fdfac92bd90c7969a7c8d29
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-14 17:30:06 -0500

    [UPD] import global firebase

--- Vue/Vuejs-Docker/vue-auth/src/main.ts
+++ Vue/Vuejs-Docker/vue-auth/src/main.ts
@@ -3,6 +3,24 @@ import App from './App.vue'
 import router from './router'
 
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap"
+
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCAYcnNeQ7VF2atqKDhTlXew1UcINyVgGA",
+    authDomain: "vuejs-5b3a4.firebaseapp.com",
+    projectId: "vuejs-5b3a4",
+    storageBucket: "vuejs-5b3a4.appspot.com",
+    messagingSenderId: "736114519531",
+    appId: "1:736114519531:web:60f6129f4b0ac75b587dae",
+    measurementId: "G-88QJQRMS2N"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
