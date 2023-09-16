bf96b1b4ca4f1c2402ca89356e4f381b7517cd30
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-16 11:14:36 -0500

    [UPD] route to component of social login

--- Vue/Vuejs-Docker/vue-auth/src/router/index.ts
+++ Vue/Vuejs-Docker/vue-auth/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthView from '../views/AuthView.vue'
 import AuthfirebaseView from '../views/AuthfirebaseView.vue'
+import SocialView from '../views/SocialView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +19,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/firebase',
     name: 'firebase',
     component: AuthfirebaseView
+  },
+  {
+    path: '/social',
+    name: 'social',
+    component: SocialView
   },
   {
     path: '/about',
