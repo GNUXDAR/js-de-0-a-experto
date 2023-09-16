e8e5aecb9c375a79b8784c8730e59d9ae6eece8a
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-16 11:12:46 -0500

    [NEW] social login - google - facebook - twitter - github

--- Vue/Vuejs-Docker/vue-auth/src/App.vue
+++ Vue/Vuejs-Docker/vue-auth/src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link :to="{name: 'auth'}">Login / Register</router-link> |
     <router-link to="/firebase">Auth with Firebase</router-link> |
+    <router-link to="/social">Auth with Social login</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
---
+++ Vue/Vuejs-Docker/vue-auth/src/views/SocialView.vue
@@ -0,0 +1,67 @@
+<template>
+    <h1>Auth View with Social login</h1>
+
+    <button class="btn btn-primary" @click="loginGoogle">Login with Google</button>
+
+    <button class="btn btn-primary" @click="loginFacebook">Login with Facebook</button>
+
+    <button class="btn btn-primary"  @click="loginTwitter">Login with Twitter</button>
+
+    <button class="btn btn-primary" @click="loginGithub">Login with Github</button>
+</template>
+
+<script lang="ts" setup>
+import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider } from "firebase/auth";
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const auth = getAuth();
+
+// Conection with google
+const loginGoogle = () => {
+    signInWithPopup(auth, googleProvider).then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        alert('Succes with Google!' + token)
+    }).catch((error) => {
+        alert('Failed with Google' + error)
+    })
+}
+
+// connection with facebook
+const loginFacebook = () => {
+    signInWithPopup(auth, facebookProvider).then((result) => {
+        const credential = FacebookAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        alert('Succes with Facebook!' + token)
+    }).catch((error) => {
+        alert('Failed with Facebook' + error)
+    })
+}
+
+// connection with twitter
+const loginTwitter = () => {
+    signInWithPopup(auth, twitterProvider).then((result) => {
+        const credential = TwitterAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        alert('Succes with Twitter!' + token)
+    }).catch((error) => {
+        alert('Failed with Twitter' + error)
+    })
+}
+
+// connection with github
+const loginGithub = () => {
+    signInWithPopup(auth, githubProvider).then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        alert('Succes with Github!' + token)
+    }).catch((error) => {
+        alert('Failed with Github' + error)
+    })
+}
+</script>
+
+<style></style>
