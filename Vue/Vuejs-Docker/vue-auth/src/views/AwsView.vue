b5ca1a3d4360c75b038daa7d23905bfc19e0799d
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-18 12:30:31 -0500

    [NEW] component to aws login

---
+++ Vue/Vuejs-Docker/vue-auth/src/views/AwsView.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Auth View with AWS</h1>
+    <authenticator>
+        <template v-slot="{user, signOut}">
+            <div>
+                <h1>Hello {{  user.username, user.email }}</h1>
+                <button @click="signOut"></button>
+                
+            </div>
+        </template>
+    </authenticator>
+</template>
+
+<script lang="ts" setup>
+import { Authenticator } from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css'
+
+</script>
+
+<style>
+</style>
