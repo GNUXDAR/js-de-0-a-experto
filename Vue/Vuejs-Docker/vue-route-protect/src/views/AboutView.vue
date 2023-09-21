0b4e17bd3b87b70eb12ddd541ff6db7767dc8dd6
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-21 17:22:38 -0500

    [UPD] protected routes

--- Vue/Vuejs-Docker/vue-route-protect/src/views/AboutView.vue
+++ Vue/Vuejs-Docker/vue-route-protect/src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Peticion Autenticada</h2>
+    <p>Aqui trabajaremos la solicitud a un endpoint y de esta manera cuando podemos acceder</p>
+
+    <h2>Protecion de Rutas</h2>
+    <p>dar acceso solo a usuarios autenticados y con permisos</p>
   </div>
 </template>
--- Vue/Vuejs-Docker/vue-route-protect/src/views/HomeView.vue
+++ Vue/Vuejs-Docker/vue-route-protect/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + Protected router"/>
   </div>
 </template>
 
