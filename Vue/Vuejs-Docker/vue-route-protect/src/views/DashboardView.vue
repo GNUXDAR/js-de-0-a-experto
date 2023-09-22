3bf587b980ff1598ca896ff786798431b8eb7dc6
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-22 15:56:18 -0500

    [NEW] component to example

---
+++ Vue/Vuejs-Docker/vue-route-protect/src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Dashboard</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
---
+++ Vue/Vuejs-Docker/vue-route-protect/src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Login</h1>
+</template>
+
+<script></script>
+
+<style scoped></style>
