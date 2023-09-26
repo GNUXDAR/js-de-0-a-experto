f7c6f9b676a0376722261d2a178e63e4ea370abb
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-26 12:03:46 -0500

    [NEW] router to components

--- Vue/Vuejs-Docker/note-project/src/router/index.ts
+++ Vue/Vuejs-Docker/note-project/src/router/index.ts
@@ -1,25 +1,79 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import LoginView from '@/views/LoginView.vue'
+import NoteCreateView from '@/views/NoteCreateView.vue'
+import NoteListView from '@/views/NoteListView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+
+// store
+import useAuth from '@/store/auth'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'noteList',
+    component: NoteListView,
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView,
+    meta: {
+      requiredAuth: false
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+    meta: {
+      requiredAuth: false
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView,
+    meta: {
+      requiredAuth: false
+    }
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: NoteCreateView,
+    meta: {
+      requiredAuth: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuth()
+  const isAuth = auth.token
+
+  if((to.meta.requiredAuth) && (isAuth == null)) {
+    next('login')
+  } else  {
+    next()
+  }
+  // const auth = false
+  // const needAuth = to.meta.requiredAuth
+
+  // if(needAuth && !auth) {
+  //   next('login')
+  // } else {
+  //   next()
+  // }
 })
 
 export default router
