356a5bef8066d36fcc912846bf872d14a4175608
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-26 12:03:15 -0500

    [UPD] import boostrap

--- Vue/Vuejs-Docker/note-project/src/main.ts
+++ Vue/Vuejs-Docker/note-project/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 const pinia = createPinia()
 const app = createApp(App)
