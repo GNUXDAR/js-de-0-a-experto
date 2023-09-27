3319348d21547dbe3bae7f2fb3aa2b380297758a
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-26 20:13:38 -0500

    [UPD] add rout with name

--- Vue/Vuejs-Docker/note-project/src/App.vue
+++ Vue/Vuejs-Docker/note-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{name: 'note'}">Home</router-link> |
+    <router-link :to="{name: 'about'}">About</router-link>
   </nav>
   <router-view/>
 </template>
