3d6e40f1184b823b1afa5384a3be8f1d30c4e501
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-28 17:11:15 -0500

    [UPD] use of layout, slots and composables in this components

--- Vue/Vuejs-Docker/vue-escalable/src/views/ContactView.vue
+++ Vue/Vuejs-Docker/vue-escalable/src/views/ContactView.vue
@@ -1,11 +1,25 @@
 <template>
     <MainLayout>
-        <h1>Contact</h1>
-    </MainLayout>
-    
+    <!-- #main es equivalente a utilizar v-slot:main -->
+    <template #main>
+        <h1>My Contact</h1>
+        <p>La posición La mouse X: {{ x }}</p> 
+        <p>La posición del mouse Y: {{ y }}</p> 
+    </template>
+    <template #aux>
+        <h3>Algo en el aux</h3>
+    </template>
+    </MainLayout>    
 </template>
 
 <script lang="ts" setup>
+import MainLayout from '@/layouts/MainLayout.vue'
+
+// uso de composable
+import { useMouse } from '@/composables/useMouse'
+
+// para usar 'x' y 'y' de useMount
+const { x, y } = useMouse()
 </script>
 
 <style>
--- Vue/Vuejs-Docker/vue-escalable/src/views/HomeView.vue
+++ Vue/Vuejs-Docker/vue-escalable/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
    <MainLayout>
-    <!-- #mainnes equivalente a utilizar v-slot:main -->
+    <!-- #main es equivalente a utilizar v-slot:main -->
     <template #main>
       <h1>My Home</h1>
+      <p>La posición La mouse X: {{ x }}</p> 
+      <p>La posición del mouse Y: {{ y }}</p> 
     </template>
     <template #aux>
       <h3>Algo en el aux</h3>
@@ -13,4 +15,10 @@
 
 <script lang="ts" setup>
 import MainLayout from '@/layouts/MainLayout.vue'
+
+// uso de composable
+import { useMouse } from '@/composables/useMouse'
+
+// para usar 'x' y 'y' de useMount
+const {x, y} = useMouse()
 </script>
