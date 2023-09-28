e5cd35a523413639ffab31d614edd82a82975101
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-28 16:21:45 -0500

    [NEW] create layout with slot named

---
+++ Vue/Vuejs-Docker/vue-escalable/src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+      <div class="container">
+        <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+        <div class="auxbar">
+          <AuxBar />
+        </div>
+        <div class="mai-content-container">
+          <div class="menu">
+            <MenuSidebar />
+          </div>
+          <div class="main-content">
+            <slot name="main"></slot>
+          </div>
+          <div class="aux-content">
+            <slot name="aux"></slot>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script lang="ts" setup>
+import MenuSidebar from '@/components/MenuSidebar.vue';
+import AuxBar from '@/components/AuxBar.vue';
+</script>
+
+<style>
+</style>
--- Vue/Vuejs-Docker/vue-escalable/src/views/HomeView.vue
+++ Vue/Vuejs-Docker/vue-escalable/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+   <MainLayout>
+    <!-- #mainnes equivalente a utilizar v-slot:main -->
+    <template #main>
+      <h1>My Home</h1>
+    </template>
+    <template #aux>
+      <h3>Algo en el aux</h3>
+    </template>
+   </MainLayout>
+
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script lang="ts" setup>
+import MainLayout from '@/layouts/MainLayout.vue'
 </script>
