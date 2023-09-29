f62ee7a0a6bc15b9d8cee1ebed846901ea8ad925
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-28 19:41:13 -0500

    [UPD] auxbar in component

--- Vue/Vuejs-Docker/vue-escalable/src/components/AuxBar.vue
+++ Vue/Vuejs-Docker/vue-escalable/src/components/AuxBar.vue
@@ -1,3 +1,44 @@
 <template>
-    <h1>Auxbar</h1>
+    <!-- DEMONSTRATION PURPOSES ONLY -->
+    <div class="auxBar">
+        <span>Need Help?</span>
+        <span><a attr.href="tel:+593963291401" title="+593963291401">+593 963 29 14 01</a> (7am to 8pm | Sat 9am-1pm EST)</span>
+    </div>
 </template>
+
+<script scoped>
+
+</script>
+
+<style scoped>
+
+.auxBar {
+  background-color: #000;
+  font: 600 14px 'Roboto', Arial, sans-serif;
+  color: #fff;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 32px;
+  box-sizing: border-box;
+
+  @media (max-width: 454px) {
+    height: auto;
+    flex-direction: column;
+  }
+
+  span {
+    mat-icon {
+      position: relative;
+      top: 2px;
+    }
+    &:not(:last-child) {
+      margin-right: 4px;
+    }
+    a {
+      color: #fff;
+    }
+  }
+}
+</style>
