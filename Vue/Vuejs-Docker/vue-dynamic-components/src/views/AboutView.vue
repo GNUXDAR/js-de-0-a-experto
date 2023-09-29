5b44d90c3e13489f85fc2127c9e947167d282dd3
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-29 14:28:03 -0500

    [UPD] description of project

--- Vue/Vuejs-Docker/vue-dynamic-components/src/views/AboutView.vue
+++ Vue/Vuejs-Docker/vue-dynamic-components/src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Permite cargar y renderizar componentes de forma dinamica en funcion de ciertas condiciones o eventos en tu aplicacion. Esto es util cuando deseas cambiar la interfaz de usuario de forma dinamica (Ejm: Modo oscuro)
+
+  Uso de componentes dinamicos, usualmente se utilizan para backoffice, mostrar data de diversas maneras o formatos.
+    </p>
+
+    <h5>Buscador con filtros por name</h5>
   </div>
 </template>
