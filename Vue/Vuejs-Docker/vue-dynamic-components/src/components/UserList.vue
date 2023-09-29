0a79708075f226fcea0eb3f1d806169b67aa5799
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-29 14:26:01 -0500

    [NEW] componente to show data od users

---
+++ Vue/Vuejs-Docker/vue-dynamic-components/src/components/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <h3>{{ props.content.name }}</h3>
+        <p>Edad: {{ props.content.age }}</p>
+        <p>Position: {{ props.content.position }}</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['content'])
+</script>
+
+<style scoped>
+.card {
+    color: #fff;
+    background-color: #34495E;
+    border-radius: 10px;
+    padding: 5px;
+    display: flex;
+    margin: 10px;
+    width: 100%;
+    justify-content: space-around;
+}
+</style>
---
+++ Vue/Vuejs-Docker/vue-dynamic-components/src/components/UserRow.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Edad</th>
+                    <th>Posición</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <h3>{{ props.content.name }}</h3>
+                    </td>
+                    <td>
+                        <p>{{ props.content.age }}</p>
+                    </td>
+                    <td>
+                        <p>{{ props.content.position }}</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['content'])
+</script>
+
+<style scoped>
+.card {
+    color: #fff;
+    background-color: #34495E;
+    border-radius: 10px;
+    padding: 5px;
+    display: flex;
+    margin: 3px;
+    width: 100%;
+    justify-content: space-around;
+}
+</style>
