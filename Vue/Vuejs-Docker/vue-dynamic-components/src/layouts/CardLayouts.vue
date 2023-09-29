a5dc52277747c1fda4b8773b0eb5ed11b2f77348
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-29 07:50:35 -0500

    [NEW] layouts to dynamic components

---
+++ Vue/Vuejs-Docker/vue-dynamic-components/src/layouts/CardLayouts.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Cars</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
---
+++ Vue/Vuejs-Docker/vue-dynamic-components/src/layouts/ListLayout.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>List</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
