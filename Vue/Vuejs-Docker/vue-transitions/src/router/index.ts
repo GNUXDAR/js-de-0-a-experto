7dcac04f27a8e5482209859beeac476c14448db7
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-30 21:05:16 -0500

    [UPD] add route to group

--- Vue/Vuejs-Docker/vue-transitions/src/router/index.ts
+++ Vue/Vuejs-Docker/vue-transitions/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GroupView from '@/views/GroupView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/group',
+    name: 'group',
+    component: GroupView
   },
   {
     path: '/about',
