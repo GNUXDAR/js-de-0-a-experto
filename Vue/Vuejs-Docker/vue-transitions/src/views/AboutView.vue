4003b404eb95706789834c71c2ae1c0c71d4b3b5
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-30 21:02:05 -0500

    [UPD] comments

--- Vue/Vuejs-Docker/vue-transitions/src/views/AboutView.vue
+++ Vue/Vuejs-Docker/vue-transitions/src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Transitions</h1>
+    <p>
+      Trabajar con transiciones nativas de <a href="https://vuejs.org/" target="_blank">vue</a> 
+    </p>
+
+    <h2>Icons</h2>
+    <p>
+      Trabajando con <a href="https://oh-vue-icons.js.org/docs" target="_blank">vue-icons</a> 
+    </p>
+
+    <code>npm install oh-vue-icons</code>
   </div>
 </template>
