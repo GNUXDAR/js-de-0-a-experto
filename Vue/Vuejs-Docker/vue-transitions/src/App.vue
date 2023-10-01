daafe4c54562ff94317cdc6c27eb4de104fff64b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-09-30 21:00:29 -0500

    [NEW] route and component to groups trnsitions

--- Vue/Vuejs-Docker/vue-transitions/src/App.vue
+++ Vue/Vuejs-Docker/vue-transitions/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link :to="{name: 'group'}">Group</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
---
+++ Vue/Vuejs-Docker/vue-transitions/src/views/GroupView.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>Transition group</h2>
+
+    <input type="text" v-model="note">
+    <button @click="handleNote">Save Note</button>
+
+    <transition-group name="list" tag="ol">
+        <li v-for="note in notes" :key="note">{{ note }}</li>
+    </transition-group>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from 'vue'
+
+    const notes = ref([])
+    const note = ref('')
+
+    const handleNote = () => {
+        notes.value.push(note.value)
+        note.value = ''
+}
+</script>
+
+<style lang="scss" scoped>
+    .list-enter-active,
+    .list-leave-active {
+        transition: all 0.5s ease;
+    }
+
+    .list-enter-from,
+    .list-leave-to {
+        opacity: 0;
+        transform: translateX(1000px);
+    }
+</style>
