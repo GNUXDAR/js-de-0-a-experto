9f588a7a3ab06acf520ab652d1bd165e0d1db26b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-02 18:01:03 -0500

    [NEW] componet to use watch with composition API

---
+++ Vue/Vuejs-Docker/vue-persistency/src/views/WatchcoView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
+
+    <h2>Uso del Watch con "Composition API"</h2>
+
+    <input type="text" v-model="note">
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+const note = ref('')
+watch(note, () => alert("Valor modificado: " + note.value))
+
+</script>
+
+<style scoped></style>
