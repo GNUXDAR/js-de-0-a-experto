f7fc1773f901dff6216fdff2074f855e71550d9d
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-02 17:58:25 -0500

    [UPD] store to use with pinia

--- Vue/Vuejs-Docker/vue-persistency/src/store/useExample.ts
+++ Vue/Vuejs-Docker/vue-persistency/src/store/useExample.ts
@@ -7,18 +7,8 @@ export const useExample = defineStore('useExample', {
         }
     },
     actions: {
-        // uso de localStorage 
-        init() {
-            const initCount = localStorage.getItem('count')
-            if(initCount) {
-                this.count = parseInt(initCount)
-            }
-        },
-        // fin de function init() para uso de localStorage 
         increment(val = 1) {
             this.count += 1
-            // uso de localStorage (nombre, valor)
-            localStorage.setItem('count', this.count.toString())
         }
     },
     // uso de pinia-plugin-persistedstate
