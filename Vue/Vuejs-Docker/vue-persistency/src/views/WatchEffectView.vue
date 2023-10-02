037dd1fa8e256831b8b2d1ac20e56dd8b9519ed4
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-02 18:01:21 -0500

    [NEW] componet to use watchEffect with composition API

---
+++ Vue/Vuejs-Docker/vue-persistency/src/views/WatchEffectView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
+
+    <h2>Uso del WatchEffect con "Composition API"</h2>
+
+    <input type="text" v-model="note">
+</template>
+
+<script lang="ts" setup>
+// option API
+import { ref, watchEffect } from 'vue'
+        const note = ref('')
+        watchEffect(()=> alert('se ha modificado el valor: ' + note.value))
+
+</script>
+
+<style scoped>
+</style>
