39b8f3288f354fb3e24159a350fda286001aa47b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-02 11:04:17 -0500

    [UPD] import piniaPluginPersistedstate

--- Vue/Vuejs-Docker/vue-persistency/src/main.ts
+++ Vue/Vuejs-Docker/vue-persistency/src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 
 const app = createApp(App)
 const pinia = createPinia()
 
+pinia.use(piniaPluginPersistedstate)
 app.use(router).use(pinia).mount('#app')
