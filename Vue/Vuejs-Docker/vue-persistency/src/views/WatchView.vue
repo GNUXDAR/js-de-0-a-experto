e932e7bb7a3b702219485bcb60fc9b27f98425d5
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-02 18:01:34 -0500

    [NEW] componet to use watch with Options API

---
+++ Vue/Vuejs-Docker/vue-persistency/src/views/WatchView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
+
+    <h2>Uso del Watch con "Options API"</h2>
+
+    <input type="text" v-model="note">
+</template>
+
+<script lang="ts">
+// option API
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data(){
+        return {
+            note: ""
+        }
+    },
+    watch: {
+        note(value, oldValue){
+            alert("Antiguo valor: " + oldValue + " Valor: " + value)
+            localStorage.setItem('note', value)
+        }
+    }
+})
+</script>
+
+<style scoped>
+</style>
