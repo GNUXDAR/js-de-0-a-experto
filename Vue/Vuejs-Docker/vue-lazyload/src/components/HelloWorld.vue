2bcc123e86c3763824525b280112795c1022a772
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-03 21:46:07 -0500

    [UPD] use of v-lazy

--- Vue/Vuejs-Docker/vue-lazyload/src/components/HelloWorld.vue
+++ Vue/Vuejs-Docker/vue-lazyload/src/components/HelloWorld.vue
@@ -1,62 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <h1>LazyLoad</h1>
+  <!-- <img v-lazy="{ src: 'https://fastly.picsum.photos/id/425/1280/800.jpg?hmac=hj4EesrT7_qptxw4AYiRxVfaQJK8VCypDx9r4srvbgw',
+    loading: 'loading.gif', error: 'loading.gif', delay: 500 }"> -->
+
+    <img v-lazy="{ src:lazyOptions.src, lifecycle: lazyOptions.lifecycle, delay: lazyOptions.delay}" lazy="loading">
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
+const lazyOptions = ref({
+  src: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI',
+  delay: 500,
+  lifecycle: {
+    loading: (el) => {
+      console.log('cargando imagen', el)
+    },
+    error: (el) => {
+      console.log('image error', el)
+    },
+    loaded: (el) => {
+      console.log('imagen cargada', el)
+    }
+  }
+})
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  img[lazy=loading] {
+    /*your style here*/
+    background-color: blue;
+    width: 500px;
+  }
+  img[lazy=error] {
+    /*your style here*/
+    background-color: red;
+    width: 500px;
+  }
+  img[lazy=loaded] {
+    /*your style here*/
+  }
 </style>
