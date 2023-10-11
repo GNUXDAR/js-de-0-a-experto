b0746366d1e61f2361d2392dde0e178ae0968019
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-11 00:11:04 -0500

    [UPD] import and config i18n

--- Vue/Vuejs-Docker/vue-31-i18n/src/main.ts
+++ Vue/Vuejs-Docker/vue-31-i18n/src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
+import { messages } from './lang/messages'
 
-createApp(App).mount('#app')
+const i18n =  createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages
+})
+
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
