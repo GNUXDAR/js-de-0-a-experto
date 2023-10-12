6a2e8f8592db79f64e9f9dc9b75196f4c76390dd
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-11 19:48:22 -0500

    [NEW] componente to translate

---
+++ Vue/Vuejs-Docker/vue-31-i18n/src/components/MenuComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <select name="" id="" v-model="$i18n.locale">
+            <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+        </select>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
