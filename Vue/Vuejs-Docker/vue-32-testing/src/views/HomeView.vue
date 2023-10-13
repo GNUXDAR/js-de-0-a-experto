9dd5268c4bcf275238bd560bee7d1f207809e33e
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-13 17:19:22 -0500

    [UPD] e2e testing

--- Vue/Vuejs-Docker/vue-32-testing/src/views/HomeView.vue
+++ Vue/Vuejs-Docker/vue-32-testing/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Welcome to Your Vue.js + TypeScript App</h1>
+
+    <h3 id="counter-info">{{ counter }}</h3>
+    <button @click="counter++">Increment</button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+const counter = ref(0)
 </script>
--- Vue/Vuejs-Docker/vue-32-testing/tests/e2e/specs/test.js
+++ Vue/Vuejs-Docker/vue-32-testing/tests/e2e/specs/test.js
@@ -5,4 +5,25 @@ describe('My First Test', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
   })
+  it('User increment counter', () => {
+    cy.visit('/')
+    cy.get('#counter-info').contains(0)
+    cy.contains('button', 'Increment').click()
+    cy.get('#counter-info').contains(1)
+  })
 })
+
+/*
+cy.visit('/') //visita una URL
+cy.url() //obtiene la URL en la que nos encontramos
+cy.contains("label", "Content") //Comprueba si la pagina contiene el elemento descrito
+cy.should('contains', 'hello cypress')  //comprobación de proposito general sobre el elemento aplicado
+cy.should('hace.css', 'color', '#ff0000') //Comprobacion de proposito general sobre el elemento aplicado
+cy.request('POST', 'uri', {}) //para realizar peticiones
+cy.get('.class') //Obtener un elemento en funcion a su clase
+cy.get('#id') //Obetner un elemento en duncion a su id
+cy.get('[data-test-id="myId"]') //Obtener un elemento en funcion a su argumento
+cy.click() //Accion: click
+cy.type() //Accion; escribir
+*/
+
