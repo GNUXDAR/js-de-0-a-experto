62d0b5596ccfb12286b28aae89bf6fdc86afbda9
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-14 17:09:13 -0500

    [NEW] service of post

---
+++ Vue/Vuejs-Docker/vue-32-testing/src/services/PostService.ts
@@ -0,0 +1,44 @@
+import InterfacePost from '@/interfaces/InterfacePost'
+import { ref, Ref } from 'vue'
+
+class PostService {
+    private post: Ref<InterfacePost> = ref<InterfacePost>({})
+    private posts: Ref<Array<InterfacePost>> = ref<Array<InterfacePost>>([])
+
+    cosntructor(){
+        // this.post = ref<InterfacePost>({})
+        // this.posts = ref<Array<InterfacePost>>([])
+    }
+
+    getPost(): Ref<InterfacePost> {
+        return this.post
+    }
+
+    getPosts(): Ref<Array<InterfacePost>> {
+        return this.posts
+    }
+
+    async fetchAll(): Promise<void> {
+        try {
+            const url = 'https://jsonplaceholder.typicode.com/posts'
+            const response = await fetch(url)
+            const json = await response.json()
+            this.posts.value = await json
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    async fetchById(id: string | Array<string>): Promise<void> {
+        try {
+            const url = `https://jsonplaceholder.typicode.com/posts/${id}`
+            const response = await fetch(url)
+            const json = await response.json()
+            this.post.value = await json
+        } catch(error) {
+            console.log(error)
+        }
+    }
+}
+
+export default PostService
