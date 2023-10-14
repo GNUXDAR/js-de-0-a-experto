a6888619a1c9a69affdb693d4e1f1c5c5db4b357
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-13 22:29:26 -0500

    [UPD] remove count

--- Vue/Vuejs-Docker/vue-32-testing/src/views/HomeView.vue
+++ Vue/Vuejs-Docker/vue-32-testing/src/views/HomeView.vue
@@ -2,14 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Welcome to Your Vue.js + TypeScript App</h1>
-
-    <h3 id="counter-info">{{ counter }}</h3>
-    <button @click="counter++">Increment</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-
-const counter = ref(0)
 </script>
