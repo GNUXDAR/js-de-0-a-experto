6347038f3041fbac2bcc190ddfa333418580d8d9
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-16 14:47:40 -0500

    [NEW] developming test

---
+++ Vue/Vuejs-Docker/vue-32-testing/tests/e2e/specs/loginTest.js
@@ -0,0 +1,24 @@
+describe('Login Test', () => {
+    it('user c an not acces to protected routed when is not logged in', ()=> {
+        cy.visit('/')
+        cy.url().should('eq', 'http://172.17.0.2:8080/login')
+    })
+
+    it('user with wrong credentials can not pass', () => {
+        cy.visit('http://172.17.0.2:8080/login')
+        cy.get('#email').type('demo@demo.com')
+        cy.get('#password').type('2023')
+        cy.get('button').click()
+        cy.contains('code', 'wrong email or password')
+        cy.visit('/')
+        cy.url().should('eq', 'http://172.17.0.2:8080/login')
+    })
+
+    it('user can successfully login', () => {
+        cy.visit('http://172.17.0.2:8080/login')
+        cy.get('#email').type('gnuxdar@admin.com')
+        cy.get('#password').type('1234567')
+        cy.get('button').click()
+        cy.url().should('eq', 'http://172.17.0.2:8080/')
+    })
+})
---
+++ Vue/Vuejs-Docker/vue-32-testing/tests/e2e/specs/postTest.js
@@ -0,0 +1,37 @@
+describe('Post test feature', () => {
+    it('Logged user can acces to page posts and views post list', () => {
+        cy.visit('http://172.17.0.2:8080/login')
+        cy.get('#email').type('gnuxdar@admin.com')
+        cy.get('#password').type('1234567')
+        cy.get('button').click()
+        cy.url().should('eq', 'http://172.17.0.2:8080/')
+        cy.contains('h1', 'Post List')
+        cy.contains('sunt aut facere repellat provident occaecati excepturi optio reprehenderit')
+    })
+
+    it('user can acces to view post detail', () => {
+        cy.visit('http://172.17.0.2:8080/login')
+        cy.get('#email').type('gnuxdar@admin.com')
+        cy.get('#password').type('1234567')
+        cy.get('button').click()
+        cy.url().should('eq', 'http://172.17.0.2:8080/')
+        cy.contains('sunt aut facere repellat provident occaecati excepturi optio reprehenderit').click()
+        cy.url('eq', 'http://172.17.0.2:8080/detail/1')
+        cy.contains('quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto')
+    })
+
+    it('Logged user can go back detail post view', () => {
+        cy.visit('http://172.17.0.2:8080/login')
+        cy.get('#email').type('gnuxdar@admin.com')
+        cy.get('#password').type('1234567')
+        cy.get('button').click()
+        cy.url().should('eq', 'http://172.17.0.2:8080/')
+        cy.contains('sunt aut facere repellat provident occaecati excepturi optio reprehenderit').click()
+        cy.url('eq', 'http://172.17.0.2:8080/detail/1')
+        cy.contains('quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto')
+        cy.get('link').click()
+        cy.url().should('eq', 'http://172.17.0.2:8080/')
+        cy.contains('h1', 'Post List')
+
+    })
+})
