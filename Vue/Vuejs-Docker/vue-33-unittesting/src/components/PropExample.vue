8b36c21abc21db71d1ad7559b3a46aa9c4ed7321
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-19 17:26:22 -0500

    [NEW] component and test with props

---
+++ Vue/Vuejs-Docker/vue-33-unittesting/src/components/PropExample.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>{{ props.title }}</h3>
+        <p>{{ props.content }}</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+const props = defineProps(['title', 'content'])
+</script>
+
+<style scoped>
+</style>
---
+++ Vue/Vuejs-Docker/vue-33-unittesting/tests/unit/propexample.spec.ts
@@ -0,0 +1,20 @@
+// testing de comunicacion vertical (props)
+import PropExample from '@/components/PropExample.vue'
+import { mount } from '@vue/test-utils'
+
+describe('PropExample.vue', ()=> {
+    it('the component receive and show title and content props', () => {
+        const wrapper = mount(PropExample, {
+            props: {
+                title: "Hello World",
+                content: "Lorem ipsum"
+            }
+        })
+        if(expect(wrapper.find('h3').exists())){
+            expect(wrapper.find('h3').text()).toBe("Hello World")
+        }
+        if(expect(wrapper.find('p').exists())){
+            expect(wrapper.find('p').text()).toBe("Lorem ipsum")
+        }
+    })
+})
