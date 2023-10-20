85bfdfec77ad3fedbdf055ebc729dac548d69b10
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-19 21:20:53 -0500

    [UPD] emit

--- Vue/Vuejs-Docker/vue-33-unittesting/src/components/PropExample.vue
+++ Vue/Vuejs-Docker/vue-33-unittesting/src/components/PropExample.vue
@@ -2,12 +2,17 @@
     <div>
         <h3>{{ props.title }}</h3>
         <p>{{ props.content }}</p>
+        <button @Click="clickMe">Click me</button>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 const props = defineProps(['title', 'content'])
+const emit = defineEmits(['clickMe'])
+const clickMe = () => {
+    emit('clickMe', 1)
+}
 </script>
 
 <style scoped>
