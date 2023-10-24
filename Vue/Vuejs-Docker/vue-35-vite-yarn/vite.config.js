bfc4d9c90e46a946d4ae5fc965b23210c38308e1
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-24 18:18:20 -0500

    [UPD] resolve to alias in routes

--- Vue/Vuejs-Docker/vue-35-vite-yarn/vite.config.js
+++ Vue/Vuejs-Docker/vue-35-vite-yarn/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    '@': path.resolve(__dirname, './src')
+  }
 })
