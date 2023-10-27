06a263643b8d67a561532a4fc2d7db70f8ca749e
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-26 20:20:18 -0500

    [NEW] dinamic params

---
+++ Vue/Vuejs-Docker/nuxt-37-routing/pages/projects/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Project: {{ projectId }}</h1>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+const projectId = ref()
+projectId.value = route.params.id
+</script>
+
+<style scoped>
+</style>
