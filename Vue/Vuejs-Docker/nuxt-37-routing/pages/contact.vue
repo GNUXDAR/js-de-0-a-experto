cea45ac5a011243ffb4288b0d97458a5aca42956
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-26 20:19:58 -0500

    [NEW] pages

---
+++ Vue/Vuejs-Docker/nuxt-37-routing/pages/contact.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Contact</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
---
+++ Vue/Vuejs-Docker/nuxt-37-routing/pages/services.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Service</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
