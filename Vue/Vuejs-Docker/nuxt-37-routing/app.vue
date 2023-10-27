d951cb0e9fee480f07bac8ef9a9fe81039501987
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-26 20:18:59 -0500

    [NEW] overwrite layuout

--- Vue/Vuejs-Docker/nuxt-37-routing/app.vue
+++ Vue/Vuejs-Docker/nuxt-37-routing/app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
---
+++ Vue/Vuejs-Docker/nuxt-37-routing/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/services">Services</NuxtLink></li>
+                <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+            </ul>
+        </nav>
+
+        <slot />
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
