c63036d47f840ea2b6538fd0590d0bece9ed7a2d
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-10-28 21:54:41 -0500

    [NEW] page to example page

---
+++ Vue/Vuejs-Docker/nuxt-37-routing/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Index</h1>
+    <h2>Title {{ note.title }}</h2>
+    <h2>Content {{ note.content }}</h2>
+</template>
+
+<script lang="ts" setup>
+import NoteService from '../services/NoteService'
+import { onMounted, ref } from 'vue';
+
+const note = ref({
+    title: 'loading...',
+    content: 'loading...' 
+})
+const noteService = new NoteService()
+
+onMounted(async () => {
+    note.value = await noteService.GetNotes()
+})
+</script>
+
+<style scoped>
+</style>
