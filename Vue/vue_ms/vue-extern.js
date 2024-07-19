a36f3efee47ca5c2fae30438785d7b5b0680bda0
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-07-19 18:53:11 -0500

    [NEW] vue file external

---
+++ Vue/vue_ms/vue-extern.js
@@ -0,0 +1,9 @@
+const App = Vue.createApp({
+    data() {
+        return {
+            firstName: 'Arturo',
+            lastName: 'Cabrera',
+            job: 'Developer, Youtuber'
+        }
+    },
+})
