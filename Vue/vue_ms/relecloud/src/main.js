3c8058bbc6d76e4d672060844241856921d705cd
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-07-23 23:28:20 -0500

    [Import, montaje] Se importa y se monta el componente

--- Vue/vue_ms/relecloud/src/main.js
+++ Vue/vue_ms/relecloud/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Host from './components/Host.vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(Host).mount("#app");
+
