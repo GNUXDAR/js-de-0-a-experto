c2fbd2e63bc061c0902a439a4e39ed3a941a6491
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-07-25 23:29:21 -0500

    [props] pruebas de props

---
+++ Vue/vue_ms/relecloud/src/components/PropsHijo.vue
@@ -0,0 +1,20 @@
+<template>
+	<button @click="count++">Me has clickeado {{ count }} veces</button><br>
+	<label for="Titulo">Titulo:</label>{{ title }} <br>
+	<label for="Descripcion">Descripcion:</label>{{ description }}
+</template>
+
+<script>
+export default {
+	props: ['title', 'description'],
+	data() {
+		return {
+			count: 0
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vue_ms/relecloud/src/components/PropsPadre.vue
@@ -0,0 +1,22 @@
+<template>
+	<h1>Aqui hay un componente hijo!</h1>
+	<props-hijo :title="title" :description="description"></props-hijo>
+</template>
+
+<script>
+import PropsHijo from "./PropsHijo.vue"
+
+export default {
+	data(){
+		return {
+			title: 'Hello world!',
+			description: 'Este es un mensaje del props Padre',
+		}
+	},
+	components: { 
+		PropsHijo 
+	}
+}
+</script>
+
+<style></style>
