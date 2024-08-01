cc27fce1a3006c737dc6c0d19352975a6e19d156
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-07-31 23:17:27 -0500

    [NEW] clone of trello

---
+++ Vue/vue_trello/src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+	<footer>
+		<span>💻 Made with ❤️</span>
+	</footer>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+footer {
+	box-sizing: border-box;
+	background-color: #CFD8DC;
+	bottom: 0;
+	left: 0;
+	padding: 1rem;
+	position: absolute;
+	width: 100%;
+	a{
+		text-decoration: none;
+		color: #546E7A;
+	}
+}
+</style>
