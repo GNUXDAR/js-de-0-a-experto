0c4eb345bd54d3d16ff4b3d719b49d307e11b99a
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-08-01 23:16:32 -0500

    [NEW] add routes of BoardView

--- Vue/vue_trello/src/router/index.js
+++ Vue/vue_trello/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LocalView from "../views/LocalView.vue";
+import BoardView from "../views/BoardView.vue";
 
 // import HomeView from '../views/HomeView.vue'
 
@@ -16,6 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/board/:id',
+    name: 'board',
+    component: BoardView,
+    props: true
   }
 ]
 
