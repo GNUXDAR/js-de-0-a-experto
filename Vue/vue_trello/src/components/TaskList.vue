e93d21f486bba7b6de9ce78b38483e87af309c29
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-08-01 23:15:05 -0500

    [NEW] component de lista de tareas

---
+++ Vue/vue_trello/src/components/TaskList.vue
@@ -0,0 +1,67 @@
+<template>
+	<ul>
+		<li v-for="(task, index) in tasks" :key="index" class="{completed: task.completed}">
+			{{ task.title }}
+		</li>
+		<input type="text" placeholder="Agregar nueva tarea" v-model="title" @keyup.enter="add()">
+	</ul>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: '',
+		}
+		
+	},
+	props: {
+		listId: String,
+		tasks: Array
+	},
+	methods: {
+		add() {
+			//eventos
+		}
+	}
+
+}
+</script>
+
+<style scoped>
+ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+}
+li {
+	background-color: #FAFAFA;
+	border-radius: 3px;
+	border-bottom: 1px solid #CCC;
+	margin:0.25rem;
+	padding: rem;
+}
+li.completed {
+	background-color: #CFD8DC;
+	color: 90A4AE;
+}
+
+input {
+	box-sizing: border-box;
+	background-color: #ECEFF1;
+	border: none;
+	border-radius: 3px;
+	color: #607D8D;
+	font-size: 1rem;
+	margin: 0 1rem;
+	outline: 0;
+	padding: 1rem;
+	transition: background-color 600ms ease;
+	width:100%;
+}
+
+input:focus,
+input:active {
+	background-color: #FAFAFA;
+}
+</style>
---
+++ Vue/vue_trello/src/views/BoardView.vue
@@ -0,0 +1,74 @@
+<template>
+	<h3>
+		<span>Mis Panales > {{ name }}</span>
+	</h3>
+	<input type="text" placeholder="Agregar una lista" v-model="listName" @keyup.enter="add()">
+
+	<div class="coantainer">
+		<column-card v-for="(list, index) in boardList" :key="index" :listId="list.id" :name="list.name"></column-card>
+	</div>
+</template>
+
+<script>
+import ColumnCard from '../components/ColumnCard.vue'
+
+export default {
+	data() {
+		return {
+			listName: '',
+			boardList: [
+				{ id: '1', name: 'Todo' },
+				{ id: '2', name: 'Doing' }
+			]
+		}
+	},
+	props: {
+		id: String,
+		name: String,
+	},
+	methods: {
+		add() {
+				this.boardList.push({ name: this.listName });
+		}
+	},
+	components: {
+		ColumnCard,
+	}
+}
+</script>
+
+<style scoped>
+section {
+	text-align: left;
+}
+
+h3 {
+	color: #37474F;
+	text-align: left;
+	margin: 1.5rem;
+}
+
+span {
+	color: #546E7A;
+}
+
+input {
+	background-color: #607D8B;
+	border: 0;
+	border-radius: 3px;
+	box-shadow: 0 0 0 0.5px rgba(49, 49, 93, .03),
+		0 2px 5px 0 rgba(49, 49, 93, .1),
+		0 1px 2px 0 rgba(0, 0, 0, .08);
+	color: #607D8D;
+	font-size: 1.2rem;
+	margin: 0 1rem;
+	outline: 0;
+	padding: 1rem;
+	transition: all 600ms ease;
+}
+
+input:focus,
+input:active {
+	background-color: #FAFAFA;
+}
+</style>
