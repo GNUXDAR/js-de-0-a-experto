3ac0faa580889e114ea8570b97ce106ad6b5cec4
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-08-03 12:08:02 -0500

    [NEW] store

---
+++ Vue/vue_trello/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    //
+  },
+  getters: {
+    //
+  },
+  mutations: {
+    //
+  },
+  actions: {
+    //
+  },
+  modules: {
+    //
+  },
+});
