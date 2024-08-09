0b9b889dffb3e359334337d88cdd0066b1949c42
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-08-08 21:33:18 -0500

    [UPD] add path to home

--- Vue/vue_components/vue_memory/src/router/index.js
+++ Vue/vue_components/vue_memory/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PrincipalView from '../views/PrincipalView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: PrincipalView
   },
   {
     path: '/about',
