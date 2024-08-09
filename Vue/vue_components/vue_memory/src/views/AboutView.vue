82510afcf7e7c894bb252c91f7b0fad8a4cadbb6
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-08-08 21:34:03 -0500

    [UPD] Benefits to memory game

--- Vue/vue_components/vue_memory/src/views/AboutView.vue
+++ Vue/vue_components/vue_memory/src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>Memory</h1>
+		<h2>Beneficios de los juegos de memoria</h2>
+		<p>Entre los beneficios que pueden otorgar los juegos de memoria para las personas encontramos:</p>
+		<ol>
+			<li>Entrenan la memoria visual.</li>
+			<li>Potencian las habilidades cognitivas.</li>
+			<li>Activan el cerebro y las habilidades cognitivas.</li>
+			<li>Mejoran la concentración.</li>
+			<li>Ayuda a conservar la memoria tanto a corto, como a medio y largo plazo.</li>
+			<li>Sirven como medio para algunos tratamientos médicos y psicológicos.</li>
+			<li>Favorecen el contacto social y la comunicación.</li>
+			<li>Aumenta la autoestima y autonomía del adulto mayor.</li>
+			<li>En definitiva, mejora su calidad de vida.</li>
+		</ol>
+	</div>
 </template>
+
+<style>
+ol {
+		text-align: center;
+		display: inline-block;
+}
+</style>
