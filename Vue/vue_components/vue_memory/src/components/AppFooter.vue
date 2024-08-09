42133a2a22c074c196529f3dfc64da016a864641
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-08-08 21:32:24 -0500

    [NEW] footer component

---
+++ Vue/vue_components/vue_memory/src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+	<footer>
+		<span>💻 Made with ❤️</span>
+	</footer>
+
+</template>
+
+<script>
+export default {
+    name: 'AppFooter',
+
+}
+</script>
+
+<style scoped>
+footer {
+	box-sizing: border-box;
+	background-color: #CFD8DC;
+	bottom: 0;
+	left: 0;
+	padding: 1rem;
+	position: absolute;
+	width: 100%;
+	a{
+		text-decoration: none;
+		color: #546E7A;
+	}
+}
+</style>
