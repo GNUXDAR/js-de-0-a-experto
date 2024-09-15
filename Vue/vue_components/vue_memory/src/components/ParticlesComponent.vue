0fee53ffb5a05122fac1d06301bd8029aec44583
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-09-14 23:37:58 -0500

    [NEW] implemented tsparticles library

---
+++ Vue/vue_components/vue_memory/src/components/ParticlesComponent.vue
@@ -0,0 +1,103 @@
+<template>
+	<div id="app">
+		<vue-particles id="tsparticles" @particles-loaded="particlesLoaded" url="http://foo.bar/particles.json" />
+
+		<vue-particles id="tsparticles" @particles-loaded="particlesLoaded" :options="{
+			background: {
+				color: {
+					value: '#0d47a1'
+				}
+			},
+			fullScreen:{
+				zIndex: -1
+			},
+			fpsLimit: 120,
+			interactivity: {
+				events: {
+					onClick: {
+						enable: true,
+						mode: 'push'
+					},
+					onHover: {
+						enable: true,
+						mode: 'repulse', //repulse, grab
+					},
+				},
+				modes: {
+					bubble: {
+						distance: 400,
+						duration: 2,
+						opacity: 0.8,
+						size: 40
+					},
+					push: {
+						quantity: 4
+					},
+					repulse: {
+						distance: 200,
+						duration: 0.4
+					}
+				}
+			},
+			particles: {
+				color: {
+					value: '#ffffff'
+				},
+				links: {
+					color: '#ffffff',
+					distance: 150,
+					enable: true,
+					opacity: 0.5,
+					width: 1
+				},
+				move: {
+					direction: 'none',
+					enable: true,
+					outModes: 'bounce',
+					random: false,
+					speed: 6,
+					straight: false
+				},
+				number: {
+					density: {
+						enable: true,
+					},
+					value: 80
+				},
+				opacity: {
+					value: 0.5
+				},
+				shape: {
+					type: [
+						'square',
+						'circle'
+					],
+				},
+				size: {
+					value: { min: 1, max: 5 }
+				}
+			},
+			detectRetina: true
+		}" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ParticleComponent',
+	data() {
+		return {
+			// 
+		}
+	},
+	methods: {
+		particlesLoaded() {
+			console.log("Particles container loaded");
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
