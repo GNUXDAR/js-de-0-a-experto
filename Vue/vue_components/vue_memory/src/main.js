9640ec55a7f232d6881b0432b511ab0a5dd8414e
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-09-15 00:33:19 -0500

    [UPD] import tsparticles

--- Vue/vue_components/vue_memory/src/main.js
+++ Vue/vue_components/vue_memory/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Particles from "@tsparticles/vue3";
+import { loadFull } from "tsparticles";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(Particles, {
+    init: async (engine) => {
+      await loadFull(engine);
+    },
+  })
+  .mount("#app");
