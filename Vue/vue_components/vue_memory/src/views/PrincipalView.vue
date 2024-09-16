4e85242f53af3ead6fee8be80d71c330c05a7d7b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-09-15 20:18:54 -0500

    [UPD] add style only view principalView to kids

--- Vue/vue_components/vue_memory/src/views/PrincipalView.vue
+++ Vue/vue_components/vue_memory/src/views/PrincipalView.vue
@@ -59,6 +59,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+	background: linear-gradient(-45deg, #52eed7, #3ce789, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	min-height: 100vh;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+
+	50% {
+		background-position: 100% 50%;
+	}
+
+	100% {
+		background-position: 0% 50%;
+	}
+}
+/* end */
 .intro, h1 {
 	color: #fff;
 	font-size: 25px;
