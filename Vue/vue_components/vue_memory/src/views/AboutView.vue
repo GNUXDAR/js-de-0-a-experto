7441e8b6b7a7ffdd651b8b93a4e86ecf2e0ae82e
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2024-09-19 12:33:51 -0500

    [UPD] add paragraph in About

--- Vue/vue_components/vue_memory/src/views/AboutView.vue
+++ Vue/vue_components/vue_memory/src/views/AboutView.vue
@@ -14,6 +14,34 @@
 			<li>Aumenta la autoestima y autonomía del adulto mayor.</li>
 			<li>En definitiva, mejora su calidad de vida.</li>
 		</ol>
+
+		<h2>Sigueme...</h2>
+		<p>
+			Mi objetivo es crear contenido valioso sobre tecnologías de desarrollo web, compartiendo mi experiencia
+			profesional y personal acumulada a lo largo de mi carrera en informática. Me siento afortunado de trabajar
+			en lo que me apasiona y quiero inspirar a otros que estudian, han estudiado o se interesan por el desarrollo
+			de software.
+		</p>
+		<p>
+			Quiero motivar a aquellos que se sienten desanimados o desorientados en su carrera, y demostrar que es
+			posible dedicarse a lo que se ama. Mi intención es contribuir con mi granito de arena y compartir mis
+			conocimientos para que todos puedan beneficiarse.
+		</p>
+		<p>
+			Considero que el desarrollo web es un campo donde todos podemos aprender y contribuir juntos, y espero que
+			mi contenido sea una fuente de inspiración y motivación para aquellos que comparten mi pasión.
+		</p>
+		<div class="icons">
+			<span class="heart-icon">❤️</span>
+			<span class="m-icon">➕</span>
+			<span class="laptop-icon">💻</span>
+			<span class="r-icon">🟰</span>
+			<span class="fly-icon">🚀</span>
+		</div>
+
+		<div class="network-media">
+			
+		</div>
 	</div>
 
 	<div class="particles">
@@ -59,5 +87,14 @@ h1 {
 
 p, ol {
 	font-size: 25px;
+}
+
+.heart-icon, .laptop-icon, .fly-icon {
+	font-size: 100px;
+}
+
+.m-icon, .r-icon {
+	font-size: 50px;
+	filter: hue-rotate(120deg);
 }
 </style>
